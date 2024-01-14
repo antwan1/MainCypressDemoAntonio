446715666b33884f5f09d79ae5c7314f9fce1ccb
antwan1 <antoniore385@gmail.com>
2024-01-14 12:45:15 +0000

    Testing the test

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    chromeWebSecurity:false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl:'https://www.saucedemo.com/',
+  
+  },
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,27 @@
+///<reference types="Cypress" />
+
+
+describe('Login', () => {
+beforeEach(() => {
+
+  cy.visit('/')
+  cy.getByClass("login_logo").should("be.visible")
+  cy.getByClass("btn_action").should("contain", "Login")
+
+
+})
+
+
+it('Standard user logs in and Logs out', () => {
+
+cy.fixture("users.json").then((userData) => {
+  cy.getById("user-name").type(userData.standardName)
+  cy.getById("password").type(userData.password)
+
+})
+cy.getById("login-button").click()
+
+
+
+})
+})
